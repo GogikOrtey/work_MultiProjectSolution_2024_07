e414449128c329cab622b626dcb4c0707cb01ddb
Georg Orlov <orlov@altek.online>
2024-12-20 10:56:05 +0500

    First commit

---
+++ source/RevitAddIn/Application.cs
@@ -0,0 +1,37 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using RevitAddIn.Commands;
+
+// ⚠️ Без VPN не запускается NuGet
+
+namespace RevitAddIn;
+
+/// <summary>
+///     Application entry point
+/// </summary>
+[UsedImplicitly]
+public class Application : ExternalApplication
+{
+    public override void OnStartup()
+    {
+        Host.Start();
+        CreateRibbon();
+    }
+
+    public override void OnShutdown()
+    {
+        Host.Stop();
+    }
+
+    private void CreateRibbon()
+    {
+        var panel = Application.CreatePanel("Commands", "RevitAddIn");
+
+        panel.AddPushButton<ShowModalWindowCommand>("Show\nModal window")
+            .SetImage("/RevitAddIn;component/Resources/Icons/RibbonIcon16.png")
+            .SetLargeImage("/RevitAddIn;component/Resources/Icons/RibbonIcon32.png");
+
+        panel.AddPushButton<ShowModelessWindowCommand>("Show\nModeless window")
+            .SetImage("/RevitAddIn;component/Resources/Icons/RibbonIcon16.png")
+            .SetLargeImage("/RevitAddIn;component/Resources/Icons/RibbonIcon32.png");
+    }
+}
